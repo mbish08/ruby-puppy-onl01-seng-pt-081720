6d524c4037b5a56bcd387e102fb434153c7c35b3
Mikel Bishop <mdrew8@gmail.com>
2020-09-07 00:49:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,9 +18,9 @@ class Dog
   end 
   
   def self.print_all
-    @@all.each do |dog|
-      puts "#{dog}" 
+    self.each do |dog|
+      puts dog.name 
     end 
   end 
-  binding.pry 
+ # binding.pry 
 end 
